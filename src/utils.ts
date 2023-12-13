abf10ca78bfe03b693c3b22827b42b153c5480da
hunghg255 <giahunghust@gmail.com>
2023-12-13 08:19:20 +0700

    fix: fix get telegram_message_thread_id

--- src/utils.ts
+++ src/utils.ts
@@ -265,7 +265,7 @@ export function getPayloadTelegram(inputs: Readonly<TInputs>): Object {
     parse_mode: 'MarkdownV2',
   };
 
-  if (inputs.telegram_message_thread_id !== "") {
+  if (inputs.telegram_message_thread_id) {
     telegram_payload = {
       ...telegram_payload,
       message_thread_id: inputs.telegram_message_thread_id
